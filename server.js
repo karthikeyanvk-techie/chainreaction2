e1de708d74853aa3136d94cc435678344eab4209
Karthikeyan-VK <karthi3259@gmail.com>
2020-03-29 01:40:59 +0530

    second

--- server.js
+++ server.js
@@ -21,11 +21,36 @@ var fs = require('fs');
 // DATABASE connection
 var mysql = require('mysql');
 var dbconfig = require('./config/database');
-var connection = mysql.createConnection({  
+
+var connection;
+
+function handleDisconnect() {
+  connection = mysql.createConnection({  
   host: "us-cdbr-iron-east-01.cleardb.net",  
   user: "b4d87862840d1f",  
   password: "61af38dd"  
-});
+}); // Recreate the connection, since
+                                                  // the old one cannot be reused.
+
+  connection.connect(function(err) {              // The server is either down
+    if(err) {                                     // or restarting (takes a while sometimes).
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+    }                                     // to avoid a hot loop, and to allow our node script to
+  });                                     // process asynchronous requests in the meantime.
+                                          // If you're also serving http, display a 503 error.
+  connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
+      handleDisconnect();                         // lost due to either server restart, or a
+    } else {                                      // connnection idle timeout (the wait_timeout
+      throw err;                                  // server variable configures this)
+    }
+  });
+}
+
+handleDisconnect();
+
 // console.log(connection);
 
 connection.query('USE ' + dbconfig.database);
